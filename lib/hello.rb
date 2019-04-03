2cec56e112c7beba12b815d560267dd7edc8b7e8
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-03 18:39:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -15,3 +15,16 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
 end
 
 # call your method here!
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given! "
+  end
+end
